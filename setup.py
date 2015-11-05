ff002c818a41b8d29ae2eb898804e745909f56e6
J. Fernando Sánchez <balkian@gmail.com>
2015-11-05 19:18:38 +0100

    Bumped to 0.4.10

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except AttributeError:
 install_reqs = [str(ir.req) for ir in install_reqs]
 test_reqs = [str(ir.req) for ir in test_reqs]
 
-VERSION = "0.4.10rc"
+VERSION = "0.4.10"
 
 setup(
     name='senpy',
