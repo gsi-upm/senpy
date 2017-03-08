c9e6d78183e887a8dae4a6c20aa82c303bce61dc
J. Fernando Sánchez <balkian@gmail.com>
2017-03-08 23:23:40 +0100

    Fixed alises, added PAD and FSRE
    Closes #13

--- senpy/plugins/conversion/centroids.py
+++ senpy/plugins/conversion/centroids.py
@@ -6,6 +6,33 @@ logger = logging.getLogger(__name__)
 
 
 class CentroidConversion(EmotionConversionPlugin):
+    def __init__(self, info):
+        if 'centroids' not in info:
+            raise Error('Centroid conversion plugins should provide '
+                        'the centroids in their senpy file')
+        if 'onyx:doesConversion' not in info:
+            if 'centroids_direction' not in info:
+                raise Error('Please, provide centroids direction')
+
+            cf, ct = info['centroids_direction']
+            info['onyx:doesConversion'] = [{
+                'onyx:conversionFrom': cf,
+                'onyx:conversionTo': ct
+            }, {
+                'onyx:conversionFrom': ct,
+                'onyx:conversionTo': cf
+            }]
+
+        if 'aliases' in info:
+            aliases = info['aliases']
+            ncentroids = {}
+            for k1, v1 in info['centroids'].items():
+                nv1 = {}
+                for k2, v2 in v1.items():
+                    nv1[aliases.get(k2, k2)] = v2
+                ncentroids[aliases.get(k1, k1)] = nv1
+            info['centroids'] = ncentroids
+        super(CentroidConversion, self).__init__(info)
 
     def _forward_conversion(self, original):
         """Sum the VAD value of all categories found."""
@@ -25,7 +52,7 @@ class CentroidConversion(EmotionConversionPlugin):
         dimensions = list(self.centroids.values())[0]
 
         def distance(e1, e2):
-            return sum((e1[k] - e2.get(self.aliases[k], 0)) for k in dimensions)
+            return sum((e1[k] - e2.get(k, 0)) for k in dimensions)
 
         emotion = ''
         mindistance = 10000000000000000000000.0
@@ -40,11 +67,12 @@ class CentroidConversion(EmotionConversionPlugin):
     def convert(self, emotionSet, fromModel, toModel, params):
 
         cf, ct = self.centroids_direction
-        logger.debug('{}\n{}\n{}\n{}'.format(emotionSet, fromModel, toModel, params))
+        logger.debug(
+            '{}\n{}\n{}\n{}'.format(emotionSet, fromModel, toModel, params))
         e = EmotionSet()
-        if fromModel == cf:
+        if fromModel == cf and toModel == ct:
             e.onyx__hasEmotion.append(self._forward_conversion(emotionSet))
-        elif fromModel == ct:
+        elif fromModel == ct and toModel == cf:
             for i in emotionSet.onyx__hasEmotion:
                 e.onyx__hasEmotion.append(self._backwards_conversion(i))
         else:
