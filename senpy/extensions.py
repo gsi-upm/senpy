f2863b6c6557d8eab489d6e831997b57d6e483a3
drevicko <drevicko@users.noreply.github.com>
2017-04-01 18:02:30 +0100

    Update extensions.py
    --use-wheel is deprecated in favour of --only-binary and is currently not present in pip master branch (with an added option :all: which could specify particular packages).
    --only-binary has been around since pip 7 or so.

--- senpy/extensions.py
+++ senpy/extensions.py
@@ -334,7 +334,8 @@ class Senpy(object):
         if requirements:
             pip_args = []
             pip_args.append('install')
-            pip_args.append('--use-wheel')
+            pip_args.append('--only-binary')
+            pip_args.append(':all:')
             for req in requirements:
                 pip_args.append(req)
             logger.info('Installing requirements: ' + str(requirements))
