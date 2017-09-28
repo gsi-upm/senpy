8ad2fae774675e26d4ca45c744ed6015488fa9e3
J. Fernando Sánchez <balkian@gmail.com>
2017-09-28 10:33:26 +0200

    More fixes to k8s recipe

--- k8s.mk
+++ k8s.mk
@@ -42,7 +42,8 @@ info: ## Print variables. Useful for debugging.
 
 
 deploy: ## Deploy to kubernetes using the credentials in KUBE_CA_PEM_FILE (or KUBE_CA_BUNDLE ) and TOKEN
-	@cat k8s/*.{yaml,yml,tmpl} | envsubst | $(KUBECTL) apply -f -
+	@ls k8s/
+	@cat k8s/*.{yaml,yml,tmpl} 2>/dev/null | envsubst | $(KUBECTL) apply -f -
 
 deploy-check: ## Get the deployed configuration.
 	@$(KUBECTL) get deploy,pods,svc,ingress
