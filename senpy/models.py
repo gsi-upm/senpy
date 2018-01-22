4af692091a39d397a0e084029b39452fd7ae1cbc
NahcoCP <ignacio.cplatas@gmail.com>
2018-01-22 11:14:30 +0100

    Adding the evaluation models

--- senpy/models.py
+++ senpy/models.py
@@ -334,5 +334,11 @@ for i in [
         'results',
         'sentimentPlugin',
         'suggestion',
+        'aggregatedEvaluation',
+        'evaluation',
+        'metric',
+        'dataset',
+        'datasets',
+
 ]:
     _add_class_from_schema(i)
